164838c54650bb897ce31ab638602b8cd1d9ae83
Brian Appleton <appleton@bu.edu>
2016-09-30 12:25:08 -0400

    functional demo program amazed this works

--- app/src/main/java/bappleton/androidapp/MainActivity.java
+++ app/src/main/java/bappleton/androidapp/MainActivity.java
@@ -15,15 +15,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
+
+import android.content.Intent;
+import android.widget.EditText;
 
 // This is brian's application
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
