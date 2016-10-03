3ba1263287f23d5438b0d3ad0a6162ac10b01e79
Brian Appleton <appleton@bu.edu>
2016-10-02 21:26:45 -0400

    removed unused function

--- app/src/main/java/bappleton/androidapp/MainActivity.java
+++ app/src/main/java/bappleton/androidapp/MainActivity.java
@@ -51,15 +51,6 @@ public class MainActivity extends AppCompatActivity {
         infoText.setText("Press NEW GAME to begin.");
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
 
     public void pressButton1(View view) {
         //user pressed button 1
